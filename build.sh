25368f5691bd0f3a444d9cd72c3b3ea1d512848f
X <xnagireddy@syena.local>
2019-06-02 09:56:35 +0530

    Resolved build issue due to missing include and lib path.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo "Rebuilding Global Domination..."
-gcc src/gdom_selftest.c src/gdom_server.c -o src/gdom_selftest -lczmq
+gcc -I/usr/local/include -L/usr/local/lib src/gdom_selftest.c src/gdom_server.c -o src/gdom_selftest -lczmq
 src/gdom_selftest
